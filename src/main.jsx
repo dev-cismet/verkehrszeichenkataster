dc6e2cd81c8f89babc648679e31a893cf3804e78
David Glogaza <david.glogaza40@googlemail.com>
2023-11-20 14:38:59 +0100

    install ant design

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ConfigProvider } from "antd";
+import locale from "antd/locale/de_DE";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider locale={locale}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
