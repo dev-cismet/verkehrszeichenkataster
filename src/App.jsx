2e1d41613ea96597736f85b25a31441c2685ca1c
David Glogaza <david.glogaza40@googlemail.com>
2023-11-24 12:35:26 +0100

    issue #003 change map size

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Map from "./components/commons/Map";
 
 function App() {
   return (
-    <div>
-      <Map key={"overview.map"} width={500} height={500} />
+    <div className="h-full max-h-[calc(100vh-73px)] w-full bg-zinc-200 p-2">
+      <Map key={"overview.map"} width={"100%"} height={500} />
     </div>
   );
 }
