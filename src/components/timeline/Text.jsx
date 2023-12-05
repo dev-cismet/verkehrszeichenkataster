a982eb3c3c2ef616ed4d89c1079333dc68364ddd
David Glogaza <david.glogaza40@googlemail.com>
2023-12-05 17:16:47 +0100

    issue #011 change textarea width

--- src/components/timeline/Text.jsx
+++ src/components/timeline/Text.jsx
@@ -3,7 +3,15 @@ import { Input } from "antd";
 const { TextArea } = Input;
 
 const Text = ({ value, id }) => {
-  return <TextArea rows={3} autoFocus defaultValue={value} id={id} />;
+  return (
+    <TextArea
+      rows={3}
+      autoFocus
+      defaultValue={value}
+      id={id}
+      className="w-1/2"
+    />
+  );
 };
 
 export default Text;
