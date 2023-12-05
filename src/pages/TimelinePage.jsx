af0c17bd7c253bd505bb52fa61c0865f7cc55ca7
David Glogaza <david.glogaza40@googlemail.com>
2023-12-05 18:28:12 +0100

    issue #011 add dropzone to timeline page

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -1,9 +1,13 @@
-import { Button, Card } from "antd";
+import { Button, Card, Upload } from "antd";
 import Timeline from "../components/application/Timeline";
 import Request from "../components/timeline/Request";
 import { useEffect, useState } from "react";
 import Text from "../components/timeline/Text";
 import Decision from "../components/timeline/Decision";
+
+import "./dragger.css";
+
+const { Dragger } = Upload;
 
 const TimelinePage = () => {
   const [timeline, setTimeline] = useState([
@@ -29,56 +33,63 @@ const TimelinePage = () => {
         overflowY: "auto",
         overflowX: "clip",
         maxHeight: "94%",
+        height: "100%",
       }}
       className="h-full w-full"
       title={<span className="text-2xl">Verlauf</span>}
     >
-      <div className="h-full w-full flex justify-between">
-        <div className="flex flex-col w-3/4">
-          {timeline.map((attachment, i) => {
-            switch (attachment.type) {
-              case "antrag":
-                return <Request key={i} />;
-              case "text":
-                return (
-                  <Text
-                    value={attachment.values?.text}
-                    id={i.toString()}
-                    key={i}
-                  />
-                );
-              case "entscheidung":
-                return <Decision id={i.toString()} />;
-            }
-          })}
-          <div className="w-full flex gap-2">
-            <Button
-              onClick={() => {
-                setTimeline((currentTimeline) => [
-                  ...currentTimeline,
-                  { type: "text" },
-                ]);
-              }}
-            >
-              Text
-            </Button>
-            <Button>Zeichnung</Button>
-            <Button
-              onClick={() =>
-                setTimeline((currentTimeline) => [
-                  ...currentTimeline,
-                  { type: "entscheidung" },
-                ])
+      <Dragger
+        openFileDialogOnClick={false}
+        className="h-full w-full"
+        onDrop={(e) => console.log(e)}
+      >
+        <div className="h-full w-full flex justify-between">
+          <div className="flex flex-col w-3/4">
+            {timeline.map((attachment, i) => {
+              switch (attachment.type) {
+                case "antrag":
+                  return <Request key={i} />;
+                case "text":
+                  return (
+                    <Text
+                      value={attachment.values?.text}
+                      id={i.toString()}
+                      key={i}
+                    />
+                  );
+                case "entscheidung":
+                  return <Decision id={i.toString()} />;
               }
-            >
-              Entscheidung
-            </Button>
+            })}
+            <div className="w-full flex gap-2">
+              <Button
+                onClick={() => {
+                  setTimeline((currentTimeline) => [
+                    ...currentTimeline,
+                    { type: "text" },
+                  ]);
+                }}
+              >
+                Text
+              </Button>
+              <Button>Zeichnung</Button>
+              <Button
+                onClick={() =>
+                  setTimeline((currentTimeline) => [
+                    ...currentTimeline,
+                    { type: "entscheidung" },
+                  ])
+                }
+              >
+                Entscheidung
+              </Button>
+            </div>
+          </div>
+          <div className="w-80">
+            <Timeline dataIn={timeline} />
           </div>
         </div>
-        <div className="w-80">
-          <Timeline dataIn={timeline} />
-        </div>
-      </div>
+      </Dragger>
     </Card>
   );
 };
