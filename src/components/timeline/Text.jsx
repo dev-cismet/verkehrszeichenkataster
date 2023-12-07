7bb4ca241e878377518baef6b14887ef8099fe15
David Glogaza <david.glogaza40@googlemail.com>
2023-12-07 18:09:49 +0100

    issue #011 change text attachment

--- src/components/timeline/Text.jsx
+++ src/components/timeline/Text.jsx
@@ -1,17 +1,46 @@
-import { Form, Input } from "antd";
+import { Input } from "antd";
+import { useDispatch } from "react-redux";
+import {
+  deleteTimelineObject,
+  updateName,
+} from "../../store/slices/application";
+import { DeleteOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
-const Text = ({ value, id }) => {
-  const [form] = Form.useForm();
+const Text = ({ attachment, id }) => {
+  const dispatch = useDispatch();
 
   return (
-    <div className="w-full">
-      <Form form={form} labelCol={{ span: 3 }} wrapperCol={{ span: 10 }}>
-        <Form.Item label="Bemerkung">
-          <TextArea rows={3} defaultValue={value} id={id} />
-        </Form.Item>
-      </Form>
+    <div className="w-full flex items-center gap-2 pb-6">
+      <div className="w-[11.5%] flex items-center justify-end">
+        <Input
+          bordered={false}
+          value={attachment.values?.name}
+          className="w-max text-end"
+          onChange={(e) => {
+            dispatch(
+              updateName({
+                index: id,
+                updatedName: e.target.value,
+              })
+            );
+          }}
+        />
+        <span>:</span>
+      </div>
+      <TextArea
+        className="w-[42%]"
+        rows={3}
+        defaultValue={attachment.values?.text}
+        id={id}
+      />
+      <DeleteOutlined
+        className="text-lg p-2 h-fit hover:bg-zinc-100 cursor-pointer rounded-lg"
+        onClick={() => {
+          dispatch(deleteTimelineObject(id));
+        }}
+      />
     </div>
   );
 };
