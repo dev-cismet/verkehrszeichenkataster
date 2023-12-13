5b7522d98902edf5db1b3aede603794a9edd50af
David Glogaza <david.glogaza40@googlemail.com>
2023-12-13 15:42:35 +0100

    issue #019 change request address form

--- src/components/timeline/Request.jsx
+++ src/components/timeline/Request.jsx
@@ -1,5 +1,4 @@
-import { Button, Input } from "antd";
-import AttachmentWrapper, { AttachmentRow } from "./AttachmentWrapper";
+import { Button, Card, Input } from "antd";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { updateTimelineValues } from "../../store/slices/application";
@@ -25,188 +24,133 @@ const Request = ({ attachment, i, isInternalRequest }) => {
   };
 
   return (
-    <AttachmentWrapper>
+    <>
       {isInternalRequest ? (
         <>
-          <AttachmentRow name="Absender">
-            {requester ? (
-              <span className="w-full">
-                {requester}{" "}
-                <EditOutlined
-                  className="cursor-pointer"
-                  onClick={() => setRequester("")}
-                />
-              </span>
-            ) : (
-              <div className="flex justify-center items-center gap-2 w-full">
-                <Button onClick={() => setRequester("104.11")}>104.11</Button>
-                <Button onClick={() => setRequester("104.23")}>104.23</Button>
-                <Button onClick={() => setRequester("GMW")}>GMW</Button>
-              </div>
-            )}
-          </AttachmentRow>
-          <AttachmentRow name="Empfänger">
-            {receiver ? (
-              <span className="w-full">
-                {receiver}{" "}
-                <EditOutlined
-                  className="cursor-pointer"
-                  onClick={() => setReceiver("")}
-                />
-              </span>
-            ) : (
-              <div className="flex justify-center items-center gap-2 w-full">
-                <Button onClick={() => setReceiver("104.11")}>104.11</Button>
-                <Button onClick={() => setReceiver("104.23")}>104.23</Button>
-                <Button onClick={() => setReceiver("GMW")}>GMW</Button>
-              </div>
-            )}
-          </AttachmentRow>
+          <Card
+            size="small"
+            type="inner"
+            title={
+              <div className="font-medium text-lg text-start">Adressen</div>
+            }
+          >
+            <div className="w-full flex flex-col gap-2">
+              {requester ? (
+                <span className="w-full">
+                  {requester}{" "}
+                  <EditOutlined
+                    className="cursor-pointer"
+                    onClick={() => setRequester("")}
+                  />
+                </span>
+              ) : (
+                <div className="flex justify-center items-center gap-2 w-full">
+                  <Button onClick={() => setRequester("104.11")}>104.11</Button>
+                  <Button onClick={() => setRequester("104.23")}>104.23</Button>
+                  <Button onClick={() => setRequester("GMW")}>GMW</Button>
+                </div>
+              )}
+              {receiver ? (
+                <span className="w-full">
+                  {receiver}{" "}
+                  <EditOutlined
+                    className="cursor-pointer"
+                    onClick={() => setReceiver("")}
+                  />
+                </span>
+              ) : (
+                <div className="flex justify-center items-center gap-2 w-full">
+                  <Button onClick={() => setReceiver("104.11")}>104.11</Button>
+                  <Button onClick={() => setReceiver("104.23")}>104.23</Button>
+                  <Button onClick={() => setReceiver("GMW")}>GMW</Button>
+                </div>
+              )}
+            </div>
+          </Card>
         </>
       ) : (
         <>
-          <div className="flex">
-            <div className="w-[22%]" />
-            <div className="w-full flex items-center justify-center gap-2">
-              <h4 className="mb-0">Antragssteller</h4>
+          <Card
+            size="small"
+            type="inner"
+            title={
+              <div className="font-medium text-lg text-start">Anfrage</div>
+            }
+          >
+            <div className="w-full flex justify-between gap-10">
+              <div className="flex flex-col gap-4 text-start w-full">
+                <span className="text-lg font-medium">Antragssteller</span>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="requester_first_name">Vorname</label>
+                    <Input id="requester_first_name" />
+                  </div>
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="requester_last_name">Nachname</label>
+                    <Input id="requester_last_name" />
+                  </div>
+                </div>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="requester_phone_number">
+                      Telefonnummer
+                    </label>
+                    <Input id="requester_phone_number" />
+                  </div>
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="reqeuster_mail">E-Mail</label>
+                    <Input id="reqeuster_mail" />
+                  </div>
+                </div>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="requester_street">Straße</label>
+                    <Input id="requester_street" />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="requester_street_number">Hausnummer</label>
+                    <Input id="requester_street_number" />
+                  </div>
+                </div>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="requester_zip_code">Postleitzahl</label>
+                    <Input id="requester_zip_code" />
+                  </div>
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="requester_city">Stadt</label>
+                    <Input id="requester_city" />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 text-start w-full">
+                <span className="text-lg font-medium">Rechnungsadresse</span>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="billing_street">Straße</label>
+                    <Input id="billing_street" />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="billing_street_number">Hausnummer</label>
+                    <Input id="billing_street_number" />
+                  </div>
+                </div>
+                <div className="flex gap-4 w-full">
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="billing_zip_coide">Postleitzahl</label>
+                    <Input id="billing_zip_coide" />
+                  </div>
+                  <div className="flex flex-col w-full gap-2">
+                    <label htmlFor="billing_city">Stadt</label>
+                    <Input id="billing_city" />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <AttachmentRow name="Stadt">
-            <Input
-              value={attachment?.requester_city}
-              onChange={(e) => {
-                updateValue(e.target.value, "requester_city");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Postleitzahl">
-            <Input
-              value={attachment?.requester_postalcode}
-              onChange={(e) => {
-                updateValue(e.target.value, "requester_postalcode");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Straße">
-            <Input
-              value={attachment?.requester_street}
-              onChange={(e) => {
-                updateValue(e.target.value, "requester_street");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Hausnummer">
-            <Input
-              value={attachment?.requester_street_number}
-              onChange={(e) => {
-                updateValue(e.target.value, "requester_street_number");
-              }}
-            />
-          </AttachmentRow>
-
-          <div className="flex">
-            <div className="w-[22%]" />
-            <div className="w-full flex items-center justify-center gap-2">
-              <h4 className="mb-0">Rechnungsadresse</h4>
-            </div>
-          </div>
-          <AttachmentRow name="Stadt">
-            <Input
-              value={attachment?.billing_city}
-              onChange={(e) => {
-                updateValue(e.target.value, "billing_city");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Postleitzahl">
-            <Input
-              value={attachment?.billing_postal_code}
-              onChange={(e) => {
-                updateValue(e.target.value, "billing_postal_code");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Straße">
-            <Input
-              value={attachment?.billing_street}
-              onChange={(e) => {
-                updateValue(e.target.value, "billing_street");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Hausnummer">
-            <Input
-              value={attachment?.billing_street_number}
-              onChange={(e) => {
-                updateValue(e.target.value, "billing_street_number");
-              }}
-            />
-          </AttachmentRow>
-
-          <div className="flex">
-            <div className="w-[22%]" />
-            <h4 className="w-full">Weiteres</h4>
-          </div>
-          <AttachmentRow name="Vorname">
-            <Input
-              value={attachment?.firstname}
-              onChange={(e) => {
-                updateValue(e.target.value, "firstname");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Nachname">
-            <Input
-              value={attachment?.lastname}
-              onChange={(e) => {
-                updateValue(e.target.value, "lastname");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Telefonnummer">
-            <Input
-              value={attachment?.phone}
-              onChange={(e) => {
-                updateValue(e.target.value, "phone");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="E-Mail">
-            <Input
-              value={attachment?.email}
-              onChange={(e) => {
-                updateValue(e.target.value, "email");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Ort des Schildes">
-            <Input
-              value={attachment?.sign_location}
-              onChange={(e) => {
-                updateValue(e.target.value, "sign_location");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Beschreibung">
-            <Input
-              value={attachment?.description}
-              onChange={(e) => {
-                updateValue(e.target.value, "description");
-              }}
-            />
-          </AttachmentRow>
-          <AttachmentRow name="Uhrzeit">
-            <Input
-              value={attachment?.time}
-              onChange={(e) => {
-                updateValue(e.target.value, "time");
-              }}
-            />
-          </AttachmentRow>
+          </Card>
         </>
       )}
-    </AttachmentWrapper>
+    </>
   );
 };
 
