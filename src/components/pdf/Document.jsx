8bbe06a8993c2b5792d8602e36e8176343c83542
David Glogaza <david.glogaza40@googlemail.com>
2024-01-11 16:28:46 +0100

    issue #034 add more text to pdf design

--- src/components/pdf/Document.jsx
+++ src/components/pdf/Document.jsx
@@ -19,13 +19,15 @@ const styles = StyleSheet.create({
 const Document = () => {
   return (
     <DocumentRenderer>
-      <Page size="A4" style={{ flexDirection: "column", padding: 20 }}>
+      <Page
+        size="A4"
+        style={{ flexDirection: "column", padding: 20, gap: 6, fontSize: 14 }}
+      >
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
           <View
             style={{
               flexDirection: "column",
               gap: 24,
-              //   alignItems: "flex-end",
             }}
           >
             <Text>104.11</Text>
@@ -49,8 +51,55 @@ const Document = () => {
           Errichtung von Verkehrszeichen und -einrichtungen gemäß § 45 Abs. 3
           StVO
         </Text>
-        <View style={{ border: 2 }}>
-          <Text>Section #2</Text>
+        <View
+          style={{
+            border: 1,
+            textAlign: "left",
+            fontSize: 16,
+            padding: 6,
+            gap: 6,
+          }}
+        >
+          <Text>Ort:</Text>
+          <Text>Lorem Ipsum</Text>
+        </View>
+        <View
+          style={{
+            border: 1,
+            textAlign: "left",
+            fontSize: 16,
+            padding: 6,
+            gap: 6,
+          }}
+        >
+          <Text>Sachverhalt:</Text>
+          <Text>Lorem Ipsum</Text>
+        </View>
+        <View
+          style={{
+            border: 1,
+            textAlign: "left",
+            fontSize: 16,
+            padding: 6,
+            gap: 6,
+          }}
+        >
+          <Text style={{ fontWeight: "heavy" }}>Erforderliche Maßnahmen:</Text>
+          <Text>Lorem Ipsum</Text>
+        </View>
+        <Text>
+          Mit der Bitte, die vorstehende aufgeführte/n Maßnahme/n nach § 45 Abs.
+          5 StVO zu veranlassen und den Tag der Durchführung auf der
+          Durchschrift mit zu teilen.
+        </Text>
+        <Text>2 - Polizeipräsident / Direktion Verkehr - z.K.</Text>
+        <Text>3 - z.V.</Text>
+        <Text>i.A.</Text>
+        <Text>Max Mustermann</Text>
+        <Text>An 104. 11</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Text>Die angeordnete/n Maßnahme/n wurden/n am</Text>
+          <Text>durchgeführt.</Text>
         </View>
       </Page>
     </DocumentRenderer>
