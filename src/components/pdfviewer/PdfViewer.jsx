70e97f54cabc80c7e3d4b24a99ac4beb601520b7
Pavel <p-olhovoi@bk.ru>
2024-01-15 14:25:31 +0100

    issue #037 change default height in pdf viewer

--- src/components/pdfviewer/PdfViewer.jsx
+++ src/components/pdfviewer/PdfViewer.jsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { pdfBase64Example } from "./pdfbase64example";
 
 const PdfViewer = ({
   filePdf = pdfBase64Example,
   width = "100%",
-  height = "700px",
+  height = "750px",
 }) => {
-  // const [createdLinks, setCreatedLinks] = useState("");
-
   const convertBase64ToUrlLink = (pdfdecoded) => {
     if (filePdf) {
       const removeBase64DataType = pdfdecoded.split(",")[1];
@@ -19,7 +17,6 @@ const PdfViewer = ({
       const byteArray = new Uint8Array(byteNumbers);
       const file = new Blob([byteArray], { type: "application/pdf" });
       const fileURL = URL.createObjectURL(file);
-      // setCreatedLinks(fileURL);
       return fileURL;
     } else {
       return "";
@@ -33,32 +30,6 @@ const PdfViewer = ({
       URL.revokeObjectURL(urlLink);
     }
   }, []);
-
-  //   useEffect(() => {
-  //     const convertBase64ToUrlLink = (pdfdecoded) => {
-  //       if (filePdf) {
-  //         const removeBase64DataType = pdfdecoded.split(",")[1];
-  //         const byteCharacters = atob(removeBase64DataType);
-  //         const byteNumbers = new Array(byteCharacters.length);
-  //         for (let i = 0; i < byteCharacters.length; i++) {
-  //           byteNumbers[i] = byteCharacters.charCodeAt(i);
-  //         }
-  //         const byteArray = new Uint8Array(byteNumbers);
-  //         const file = new Blob([byteArray], { type: "application/pdf" });
-  //         const fileURL = URL.createObjectURL(file);
-  //         setCreatedLinks(fileURL);
-  //         return fileURL;
-  //       } else {
-  //         return "";
-  //       }
-  //     };
-
-  //     convertBase64ToUrlLink(filePdf);
-
-  //     if (createdLinks !== "") {
-  //       URL.revokeObjectURL(createdLinks);
-  //     }
-  //   }, [filePdf]);
 
   return <iframe title="PDF Viewer" src={urlLink} style={{ width, height }} />;
 };
