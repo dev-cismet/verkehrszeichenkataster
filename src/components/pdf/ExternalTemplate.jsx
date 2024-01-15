6dd4892213cdc1bd17571bf5504807b6d717e3e7
David Glogaza <david.glogaza40@googlemail.com>
2024-01-15 16:57:10 +0100

    issue #034 show real data in pdf

--- src/components/pdf/ExternalTemplate.jsx
+++ src/components/pdf/ExternalTemplate.jsx
@@ -3,13 +3,31 @@ import {
   Text,
   View,
   Document,
-  StyleSheet,
   Image,
 } from "@react-pdf/renderer";
 import ExternalSidebar from "./components/ExternalSidebar";
 import Contact from "./components/Contact";
+import { mdParser } from "../mdredactor/MdRedactor";
 
-const ExternalTemplate = () => {
+
+const TextWithTitle = ({ title, text }) => {
+  return (
+    <View
+      style={{
+        textAlign: "left",
+        fontSize: 14,
+        gap: 6,
+      }}
+    >
+      <Text style={{ textDecoration: "underline", paddingTop: 10 }}>
+        {title}
+      </Text>
+      <Text style={{ maxWidth: "70%" }}>{text}</Text>
+    </View>
+  );
+};
+
+const ExternalTemplate = ({ timeline, title }) => {
   return (
     <Document>
       <Page
@@ -27,6 +45,20 @@ const ExternalTemplate = () => {
             <Text>Wupperwurm</Text>
           </View>
           <Contact />
+          <View style={{ textAlign: "right" }}>
+            <Text>15.01.2024</Text>
+          </View>
+          <Text style={{ textDecoration: "underline" }}>{title}</Text>
+          <Text style={{ fontSize: 10 }}>(Anordnung Nr. 001/2024)</Text>
+          {timeline?.map((attachment) => {
+            if (attachment.typ === "text") {
+              return (
+                <TextWithTitle title={attachment.name} text={attachment.text} />
+              );
+            } else if (attachment.typ === "file") {
+              return <Image src={attachment.file} />;
+            }
+          })}
         </View>
         <ExternalSidebar />
       </Page>
