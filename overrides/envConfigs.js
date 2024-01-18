a3a7640ea6d7cc9304a9a0f73ed62f18cb8990ec
d4v3000 <d4v3000@users.noreply.github.com>
2024-01-18 18:27:02 +0000

    deploy: 58be717c799f0400dfd50a45c7eeaa82f671e3c2

---
+++ overrides/envConfigs.js
@@ -0,0 +1,7 @@
+window.VZKAT_ONLINE_ENV_CONFIG = {
+  REST_SERVICE: "https://wunda-cloud-api.cismet.de",
+  OFFLINE_ACTIONS_ROUTE: "offline-actions-wunda-cloud.cismet.de/v1/graphql",
+  DB_VERSION: "vzk-online-cloud-db-1.0",
+  DOMAIN: "WUNDA_BLAU",
+  PLAYGROUND: true,
+};
