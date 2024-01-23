bdc8f0d8042f36df41ef870a98a73590fe95ff92
David Glogaza <david.glogaza40@googlemail.com>
2024-01-23 12:52:18 +0100

    show submit card when anordnung is closed

--- src/components/timeline/SubmitCard.jsx
+++ src/components/timeline/SubmitCard.jsx
@@ -37,188 +37,187 @@ const SubmitCard = ({ changeTimeline, handleDrop }) => {
 
   return (
     <>
-      {status === "Offen" && (
-        <div className="flex flex-col gap-2 w-full py-4">
-          <span className="text-start text-lg font-medium">
-            Anhang Hinzufügen
-          </span>
-          <Card size="small" type="inner">
-            <div className="flex flex-col gap-2">
-              <Input
-                placeholder="Name"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
+      <div className="flex flex-col gap-2 w-full py-4">
+        <span className="text-start text-lg font-medium">
+          Anhang Hinzufügen
+        </span>
+        <Card size="small" type="inner">
+          <div className="flex flex-col gap-2">
+            <Input
+              placeholder="Name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+            {useDrawing ? (
+              <Designer
+                getElements={(elements) => setDrawElements(elements)}
+                getFiles={(files) => setDrawFiles(files)}
+                initialElements={drawElements}
+                resetDrawing={triggerDrawingGeneration}
+                getPreviewSrcLink={(preview) => setDrawing(preview)}
               />
-              {useDrawing ? (
-                <Designer
-                  getElements={(elements) => setDrawElements(elements)}
-                  getFiles={(files) => setDrawFiles(files)}
-                  initialElements={drawElements}
-                  resetDrawing={triggerDrawingGeneration}
-                  getPreviewSrcLink={(preview) => setDrawing(preview)}
-                />
-              ) : (
-                <MdRedactor getDocument={(text) => setText(text)} />
-              )}
-              <div className="flex items-center gap-4">
-                {!useDrawing && (
-                  <>
-                    <Upload
-                      beforeUpload={(file) => {
-                        handleDrop(file);
-                      }}
-                      fileList={[]}
-                    >
-                      <Button className="w-fit" icon={<FileAddOutlined />}>
-                        Datei
-                      </Button>
-                    </Upload>
-                    <Button
-                      className="w-fit"
-                      icon={<HighlightOutlined />}
-                      onClick={() => {
-                        setUseDrawing(true);
-                        if (submitRef.current) {
-                          setTimeout(() => {
-                            submitRef.current.scrollIntoView({
-                              behavior: "smooth",
-                            });
-                          }, 5);
-                        }
-                      }}
-                    >
-                      Zeichnung
+            ) : (
+              <MdRedactor getDocument={(text) => setText(text)} />
+            )}
+            <div className="flex items-center gap-4">
+              {!useDrawing && (
+                <>
+                  <Upload
+                    beforeUpload={(file) => {
+                      handleDrop(file);
+                    }}
+                    fileList={[]}
+                  >
+                    <Button className="w-fit" icon={<FileAddOutlined />}>
+                      Datei
                     </Button>
-                  </>
-                )}
-              </div>
+                  </Upload>
+                  <Button
+                    className="w-fit"
+                    icon={<HighlightOutlined />}
+                    onClick={() => {
+                      setUseDrawing(true);
+                      if (submitRef.current) {
+                        setTimeout(() => {
+                          submitRef.current.scrollIntoView({
+                            behavior: "smooth",
+                          });
+                        }, 5);
+                      }
+                    }}
+                  >
+                    Zeichnung
+                  </Button>
+                </>
+              )}
             </div>
-          </Card>
-          <div className="w-full flex items-center gap-2 justify-end">
-            {useDrawing && (
-              <Button
-                onClick={() => {
-                  setUseDrawing(false);
-                }}
-                icon={<CloseOutlined />}
-              >
-                Abbrechen
-              </Button>
-            )}
-            {status === "Offen" && (
-              <Button
-                type="primary"
-                onClick={() => {
-                  setTriggerDrawingGeneration((prevValue) => {
-                    return prevValue + 1;
-                  });
-                  const uuid = uuidv4();
-                  const timelineObjectId = uuidv4();
-
-                  if (useDrawing) {
-                    dispatch(
-                      addAnordnungAction({
-                        className: "vzk_attachment_drawing",
-                        data: {
-                          drawing: JSON.stringify({
-                            elements: drawElements,
-                            files: drawFiles,
-                          }),
-                          uuid: uuid,
-                        },
-                      })
-                    );
-                    dispatch(
-                      addAnordnungAction({
-                        className: "vzk_anordnung",
-                        data: {
-                          uuid: id,
-                          vzk_anordnung_timelineArrayRelationShip: [
-                            ...anordnung.vzk_anordnung_timelineArrayRelationShip,
-                            {
-                              name: name,
-                              fk_uuid: uuid,
-                              uuid: timelineObjectId,
-                              vzk_attachment_typ: {
-                                id: 5,
-                                name: "Drawing",
-                              },
-                            },
-                          ],
-                        },
-                      })
-                    );
-
-                    changeTimeline({
-                      typ: "drawing",
-                      name: name,
-                      fk_uuid: uuid,
-                      uuid: timelineObjectId,
-                      vzk_attachment_typ: {
-                        id: 5,
-                        name: "Drawing",
-                      },
-                      data: {
-                        drawing: JSON.stringify({
-                          elements: drawElements,
-                          files: drawFiles,
-                        }),
-                      },
-                    });
-                  } else {
-                    dispatch(
-                      addAnordnungAction({
-                        className: "vzk_attachment_text",
-                        data: {
-                          text: text,
-                          uuid: uuid,
-                        },
-                      })
-                    );
-                    dispatch(
-                      addAnordnungAction({
-                        className: "vzk_anordnung",
-                        data: {
-                          uuid: id,
-                          vzk_anordnung_timelineArrayRelationShip: [
-                            ...anordnung.vzk_anordnung_timelineArrayRelationShip,
-                            {
-                              name: name,
-                              fk_uuid: uuid,
-                              uuid: timelineObjectId,
-                              vzk_attachment_typ: {
-                                id: 2,
-                                name: "Text",
-                              },
-                            },
-                          ],
-                        },
-                      })
-                    );
-                    changeTimeline({
-                      vzk_attachment_typ: {
-                        id: 2,
-                        name: "Text",
-                      },
-                      name: name,
-                      data: { text: text },
-                    });
-                  }
-                  setText("");
-                  setName("");
-                  setDrawElements([]);
-                  setUseDrawing(false);
-                }}
-                disabled={!text && !(drawElements.length >= 0)}
-                icon={<PlusOutlined />}
-                ref={submitRef}
-              >
-                Hinzufügen
-              </Button>
-            )}
           </div>
+        </Card>
+        <div className="w-full flex items-center gap-2 justify-end">
+          {useDrawing && (
+            <Button
+              onClick={() => {
+                setUseDrawing(false);
+              }}
+              icon={<CloseOutlined />}
+            >
+              Abbrechen
+            </Button>
+          )}
+
+          <Button
+            type="primary"
+            onClick={() => {
+              setTriggerDrawingGeneration((prevValue) => {
+                return prevValue + 1;
+              });
+              const uuid = uuidv4();
+              const timelineObjectId = uuidv4();
+
+              if (useDrawing) {
+                dispatch(
+                  addAnordnungAction({
+                    className: "vzk_attachment_drawing",
+                    data: {
+                      drawing: JSON.stringify({
+                        elements: drawElements,
+                        files: drawFiles,
+                      }),
+                      uuid: uuid,
+                    },
+                  })
+                );
+                dispatch(
+                  addAnordnungAction({
+                    className: "vzk_anordnung",
+                    data: {
+                      uuid: id,
+                      vzk_anordnung_timelineArrayRelationShip: [
+                        ...anordnung.vzk_anordnung_timelineArrayRelationShip,
+                        {
+                          name: name,
+                          fk_uuid: uuid,
+                          uuid: timelineObjectId,
+                          vzk_attachment_typ: {
+                            id: 5,
+                            name: "Drawing",
+                          },
+                        },
+                      ],
+                    },
+                  })
+                );
+
+                changeTimeline({
+                  typ: "drawing",
+                  name: name,
+                  fk_uuid: uuid,
+                  uuid: timelineObjectId,
+                  vzk_attachment_typ: {
+                    id: 5,
+                    name: "Drawing",
+                  },
+                  data: {
+                    drawing: JSON.stringify({
+                      elements: drawElements,
+                      files: drawFiles,
+                    }),
+                  },
+                });
+              } else {
+                dispatch(
+                  addAnordnungAction({
+                    className: "vzk_attachment_text",
+                    data: {
+                      text: text,
+                      uuid: uuid,
+                    },
+                  })
+                );
+                dispatch(
+                  addAnordnungAction({
+                    className: "vzk_anordnung",
+                    data: {
+                      uuid: id,
+                      vzk_anordnung_timelineArrayRelationShip: [
+                        ...anordnung.vzk_anordnung_timelineArrayRelationShip,
+                        {
+                          name: name,
+                          fk_uuid: uuid,
+                          uuid: timelineObjectId,
+                          vzk_attachment_typ: {
+                            id: 2,
+                            name: "Text",
+                          },
+                        },
+                      ],
+                    },
+                  })
+                );
+                changeTimeline({
+                  vzk_attachment_typ: {
+                    id: 2,
+                    name: "Text",
+                  },
+                  name: name,
+                  data: { text: text },
+                });
+              }
+              setText("");
+              setName("");
+              setDrawElements([]);
+              setUseDrawing(false);
+            }}
+            disabled={!text && !(drawElements.length >= 0)}
+            icon={<PlusOutlined />}
+            ref={submitRef}
+          >
+            {status === "Offen"
+              ? "Hinzufügen"
+              : "Wieder eröffnen und hinzufügen"}
+          </Button>
         </div>
-      )}
+      </div>
     </>
   );
 };
